184441bd94b26815083f29a4114e6550bff72a19
jinho3085 <jinho3085@naver.com>
2025-08-18 17:33:23 +0900

    practice 01

--- sql pratices/practice01.sql
+++ sql pratices/practice01.sql
@@ -2,16 +2,24 @@
 
 -- 문제1.
 -- 사번이 10944인 사원의 이름은(전체 이름)
-
+select concat(first_name, ' ', last_name) as full_name
+	from employees
+    where emp_no = 10944; -- Remko Zyda
 
 -- 문제2. 
 -- 전체 직원의 다음 정보를 조회하세요. 가장 선임부터 출력이 되도록 하세요.
 -- 출력은 이름, 성별, 입사일 순서이고 "이름", "성별", "입사일"로 컬럼 이름을 대체해 보세요.
-
+select concat(first_name, ' ', last_name) as '이름',
+		gender as '성별',
+        hire_date as '입사일'
+        from employees
+        order by hire_date asc;
 
 -- 문제3.
 -- 여직원과 남직원은 각 각 몇 명이나 있나요?(각각 쿼리 만들어서 각각 출력 또는 집계로 한 번에 해결)
-
+select gender, count(*) as 인원수
+from employees
+group by gender;
 
 -- 문제4.
 -- 현재(to_date='9999-01-01'), 근무하고 있는 직원 수는 몇 명입니까? (salaries 테이블을 사용) 
