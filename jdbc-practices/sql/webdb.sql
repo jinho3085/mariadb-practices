584ddadf7454e3bc768492fbc9d6e2ab07ee2f0a
jinho3085 <jinho3085@naver.com>
2025-08-19 17:17:01 +0900

    driver, email,example

---
+++ jdbc-practices/sql/webdb.sql
@@ -0,0 +1,34 @@
+--
+-- JDBC Test SQL
+--
+
+desc dept;
+
+-- select 
+select id, name from dept where name like '%개발%';
+
+-- insert
+insert into dept(name) values('UX팀');
+
+-- delete
+delete from dept where id = 7;
+
+-- update
+update dept set name = '서비스개발팀' where id = 2;
+
+--
+-- email application
+-- 
+
+desc email;
+alter table email change id id int auto_increment;
+
+-- findAll
+select id, first_name, last_name, email from email order by id desc;
+
+-- deleteByEmail
+delete from email where email = 'dooly@gamil.com';
+
+-- insert
+insert into email(first_name, last_name, email) values ('둘','리', 'dooly@gmail.com');
+
